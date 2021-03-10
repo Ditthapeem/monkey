938355fb02a266f6971fd58b7d1f7484a58b2e7a
HuskyIsHere <72660373+HuskyIsHere@users.noreply.github.com>
2021-03-10 12:57:17 +0700

    Extract sprite class
    Resolve #1

--- monkey.py
+++ monkey.py
@@ -4,6 +4,20 @@ import tkinter.ttk as ttk
 CANVAS_WIDTH = 800
 CANVAS_HEIGHT = 500
 
+class Sprite():
+
+    def __init__(self, canvas, image_filename, x=0, y=0):
+        self.image_filename = image_filename
+        self.x = x
+        self.y = y
+        self.canvas = canvas
+
+        self.init_canvas_object()
+
+    def init_canvas_object(self):
+        self.photo_image = tk.PhotoImage(file=self.image_filename)
+        self.canvas_object_id = self.canvas.create_image(self.x, self.y, image=self.photo_image)
+
 
 class MonkeyGame(ttk.Frame):
 
@@ -11,14 +25,18 @@ class MonkeyGame(ttk.Frame):
         super().__init__(parent)
         self.grid(sticky="NEWS")
         self.create_widgets()
+        
+        self.create_sprite()
 
     def create_widgets(self):
         self.canvas = tk.Canvas(self, borderwidth=0, width=CANVAS_WIDTH,
                                 height=CANVAS_HEIGHT, highlightthickness=0)
         self.canvas.grid(sticky='NEWS')
 
-        self.banana_image = tk.PhotoImage(file=r'C:/Work/Ske Sem2 2564/Computer Programming II 01219116/Lesson/Week 7/monkey/Banana2.png')
-        self.banana = self.canvas.create_image(100, 100, image=self.banana_image)
+    def create_sprite(self):
+        self.banana = Sprite(self.canvas, "Banana2.png", 100, 100)
+
+
 
 if __name__ == "__main__":
     root = tk.Tk()

abc4328149e773f3301ee56ed832525b5fafac62
HuskyIsHere <72660373+HuskyIsHere@users.noreply.github.com>
2021-03-10 19:56:25 +0700

    Extract GameApp and Sprite to game lib

--- monkey.py
+++ monkey.py
@@ -1,79 +1,12 @@
 import tkinter as tk
-import tkinter.ttk as ttk
+
+from gamelib import Sprite, GameApp
 
 CANVAS_WIDTH = 800
 CANVAS_HEIGHT = 500
 
 UPDATE_DELAY = 33
 GRAVITY = 1
-
-class Sprite():
-
-    def __init__(self, canvas, image_filename, x=0, y=0):
-        self.image_filename = image_filename
-        self.x = x
-        self.y = y
-        self.canvas = canvas
-
-        self.init_canvas_object()
-        self.init_sprite()
-
-    def init_sprite(self):
-        pass
-
-    def init_canvas_object(self):
-        self.photo_image = tk.PhotoImage(file=self.image_filename)
-        self.canvas_object_id = self.canvas.create_image(self.x, self.y, image=self.photo_image)
-
-    def render(self):
-        self.canvas.coords(self.canvas_object_id, self.x, self.y)
-
-    def update(self):
-        pass
-
-
-class GameApp(ttk.Frame):
-
-    def __init__(self, parent):
-        super().__init__(parent)
-        self.grid(sticky="NEWS")
-        self.create_canvas()
-
-        self.sprite = []
-        self.init_game()
-
-    def create_canvas(self):
-        self.canvas = tk.Canvas(self, borderwidth=0, width=CANVAS_WIDTH,
-                                height=CANVAS_HEIGHT, highlightthickness=0)
-        self.canvas.grid(sticky='NEWS')
-
-    def animate(self):
-        for sprite in self.sprite:
-            sprite.update()
-            sprite.render()
-
-        self.after(UPDATE_DELAY, self.animate)
-
-    def start(self):
-        self.after(0, self.animate)
-
-    def init_game(self):
-        pass
-
-
-class MonkeyGame(GameApp):
-    def create_sprite(self):
-        self.banana = Banana(self.canvas, "Banana.png", 100, 400)
-        self.banana.set_speed(15, 25)
-
-        self.sprite.append(self.banana)
-
-        self.monkey = Sprite(self.canvas, 'gollira.png', 100, 400)
-
-    def init_game(self):
-        self.create_sprite()
-
-
 
 class Banana(Sprite):
     def init_sprite(self):
@@ -90,12 +23,26 @@ class Banana(Sprite):
         self.vy -= GRAVITY
 
 
+class MonkeyGame(GameApp):
+    def create_sprite(self):
+        self.banana = Banana(self, "Banana.png", 100, 400)
+        self.banana.set_speed(15, 25)
+
+        self.monkey = Sprite(self, 'gollira.png', 100, 400)
+
+        self.sprite.append(self.banana)
+        self.sprite.append(self.monkey)
+
+    def init_game(self):
+        self.create_sprite()
+
+
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("Monkey Game")
 
     root.resizable(False, False)
-    app = MonkeyGame(root)
+    app = MonkeyGame(root, CANVAS_WIDTH, CANVAS_HEIGHT, UPDATE_DELAY)
     app.start()
     root.mainloop()
 
